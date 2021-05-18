e0e79fe39b89313c0342d623c0a245c65f5cc531
Landon Turner <lturner@teamworks.com>
2021-05-18 07:58:34 -0400

    basic styled components

--- frontend/web/components/Header.js
+++ frontend/web/components/Header.js
@@ -1,10 +1,27 @@
 import Nav from "./Nav";
 
+const Title = styled.h1`
+    font-size: 4rem;
+    margin-left: 2rem;
+    position: relative;
+    z-index: 2;
+    transfrom: skew(-7deg);
+    background: black;
+    a {
+        color: white
+        text-transform: uppercase;
+        padding: 0.5rem 1rem;
+    }
+`
+
 export default function Header(){
     return (
         <header>
             <div id="title" className="bar">
-                <Link href="/">Studio 9 NC</Link>
+                <Title>
+                    <Link href="/">Studio 9 NC</Link>
+                </Title>
+                
             </div>
             <Nav />
         </header>
