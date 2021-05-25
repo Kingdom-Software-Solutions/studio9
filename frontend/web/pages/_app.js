5f9171787060b893c0ea24949710b79bbb8f92a1
Landon Turner <lturner@teamworks.com>
2021-05-25 09:51:37 -0400

    add styles and nprogress bar

--- frontend/web/pages/_app.js
+++ frontend/web/pages/_app.js
@@ -1,4 +1,14 @@
-import '../styles/globals.css'
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import '../styles/globals.css';
+// TODO: swap with my own css to customize progress bar
+import 'nprogress/nprogress.css';
+// import '../components/styles/nprogress.css';
+
+// Sets progress bar in app
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
